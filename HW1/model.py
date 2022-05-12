1be0897822f0a0ecc0d8c84f7e600481201acfce
Liron <lironpaz7@gmail.com>
2022-05-12 21:08:10 +0300

    ''

--- HW1/model.py
+++ HW1/model.py
@@ -5,13 +5,13 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=5, padding=2),
+            nn.Conv2d(3, 16, kernel_size=8, padding=4),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2))
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.Conv2d(16, 32, kernel_size=8, padding=4),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2))
